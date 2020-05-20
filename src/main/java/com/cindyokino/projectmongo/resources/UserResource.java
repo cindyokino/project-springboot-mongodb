352a639cbb8ce611cdffdebffac44be07b6a4805
Cindy Okino <cindy.okino@gmail.com>
2020-05-20 18:05:05 -0400

    Getting a user by id

--- src/main/java/com/cindyokino/projectmongo/resources/UserResource.java
+++ src/main/java/com/cindyokino/projectmongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,13 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto); // instanciar ResponseEntity<T>
+	}	
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) // indica que o método vai ser o endpoint REST no caminho /users
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) { // ResponseEntity<T> encapsula para retornar respostas HTTP
+		User obj = service.findById(id);		
+		return ResponseEntity.ok().body(new UserDTO(obj)); // instanciar ResponseEntity<T>
 	}
+	
+	
 }
---
+++ src/main/java/com/cindyokino/projectmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.cindyokino.projectmongo.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.cindyokino.projectmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice // significa que esta classe é responsavel por tratar possiveis erros nas minhas requisicoes
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Not found", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
