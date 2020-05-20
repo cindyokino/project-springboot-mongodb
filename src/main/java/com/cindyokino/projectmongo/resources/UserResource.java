19420b9e4a5f60cad4518ceac3a6ba2fa4442957
Cindy Okino <cindy.okino@gmail.com>
2020-05-20 17:26:52 -0400

    Using DTO to return users

--- src/main/java/com/cindyokino/projectmongo/resources/UserResource.java
+++ src/main/java/com/cindyokino/projectmongo/resources/UserResource.java
@@ -1,8 +1,7 @@
 package com.cindyokino.projectmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cindyokino.projectmongo.domain.User;
+import com.cindyokino.projectmongo.dto.UserDTO;
 import com.cindyokino.projectmongo.services.UserService;
 
 @RestController // indica que a classe é um recurso REST
@@ -21,8 +21,9 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET) // indica que o método vai ser o endpoint REST no caminho /users
-	public ResponseEntity<List<User>> findAll() { // ResponseEntity<T> encapsula para retornar respostas HTTP
+	public ResponseEntity<List<UserDTO>> findAll() { // ResponseEntity<T> encapsula para retornar respostas HTTP
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list); // instanciar ResponseEntity<T>
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto); // instanciar ResponseEntity<T>
 	}
 }
